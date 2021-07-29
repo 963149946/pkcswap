e0a873018c1de2f6aacec3a1db1ddeb03d1dacca
963149946 <963149946@qq.com>
2021-07-29 13:17:53 +0800

    modify tokenlist port via baota panel

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -6,6 +6,9 @@ export const DEFAULT_TOKEN_LIST_URL = 'http://raw.githubusercontent.com/96314994
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-  'http://119.29.91.158:80/tokenlist'
+  //宝塔面板默认使用8888端口，apache默认使用80端口
+  //宝塔面板的左侧导航栏安全页面可以设置放行端口范围3000-5000
+  //这里不知道为啥腾讯云的安全组页面放行端口没有效果，要借助宝塔才行
+  'http://119.29.91.158:3333/tokenlist'
 
 ]
